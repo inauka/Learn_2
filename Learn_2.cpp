6c08ea2936148d36c12e8c468c46742dba4cb8e3
Dmitry Korytniy <kor_dima@inbox.ru>
2021-03-04 11:05:02 +0300

    Добавьте файлы проекта.

---
+++ Learn_2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include "Helpers.h"
+
+
+
+
+int main()
+{
+   
+    setlocale(LC_ALL, "Rus");
+
+    int a;
+    int b;
+
+    std::cout << "введите число 1: ";
+    std::cin >> b;
+    std::cout << "введите число 2: ";
+    std::cin >> a;
+   
+  int с = summa(a, b);
+    
+   
+    std::cout << "квадрат суммы " << с << std::endl;
+}
+
