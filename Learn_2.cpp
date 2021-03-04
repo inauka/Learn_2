5433ddff1ccb82c2db8d8848824289b94bc06e8c
Dmitry Korytniy <kor_dima@inbox.ru>
2021-03-04 11:39:58 +0300

    v2

--- Learn_2.cpp
+++ Learn_2.cpp
@@ -2,22 +2,20 @@
 #include <iostream>
 #include "Helpers.h"
 
-
-
-
 int main()
 {
    
     setlocale(LC_ALL, "Rus");
 
-    int a;
-    int b;
+    int a = 3;
+    int b = 5;
 
-    std::cout << "введите число 1: ";
+    /*std::cout << "введите число 1: ";
     std::cin >> b;
     std::cout << "введите число 2: ";
     std::cin >> a;
-   
+   */
+
   int с = summa(a, b);
     
    
